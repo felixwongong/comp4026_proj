bdd253979840e40f595f9b686fc5edae12e5a5fe
felixwongong <74048524+felixwongong@users.noreply.github.com>
2023-04-07 18:44:41 +0800

    fix station ui

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import cv2 as cv
 import torchxrayvision as xrv
 import matplotlib.pyplot as plt
+import pandas as pd
+from tabulate import tabulate
 
 def main():
-    d = xrv.datasets.COVID19_Dataset(imgpath="covid-chestxray-dataset-master/images", csvpath="covid-chestxray-dataset-master/metadata.csv")
-    img = d[2]["img"][0];
+    dataset = xrv.datasets.COVID19_Dataset(imgpath="covid-chestxray-dataset-master/images", csvpath="covid-chestxray-dataset-master/metadata.csv")
+    img = dataset[2]["img"][0];
 
     plt.imshow(img, cmap="gray")
     cv.waitKey(0)
     cv.destroyAllWindows()
     plt.show()
+
+    csv = pd.DataFrame(dataset.csv)
+    print(tabulate(csv.head(), headers='keys'))
 
 if __name__ == "__main__":
     main()
