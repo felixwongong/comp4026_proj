1ff2965231fd3ae6e69c1452b70fab08c898e020
felixwongong <74048524+felixwongong@users.noreply.github.com>
2023-04-16 13:55:02 +0800

    add image augmentation

--- main.py
+++ main.py
@@ -5,16 +5,38 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from tabulate import tabulate
 from PIL import Image
+import imgaug.augmenters as iaa
+from imgaug.augmentables.segmaps import SegmentationMapsOnImage
 
 # https://github.com/ieee8023/covid-chestxray-dataset
 
 width = 200
 height = 200
+augment_seq = iaa.Sequential([
+    iaa.Fliplr(0.5),
+    iaa.Affine(
+        rotate=(-20, 20),
+        scale=(0.8, 1.2),
+    ),
+    iaa.Multiply((0.8, 1.2)),
+    iaa.GaussianBlur(sigma=(0, 2.0)),
+], random_order=True)
+
+
+
+def augment_image(image, n_augments = 1):
+    image = np.clip(image * 255, 0, 255).astype(np.uint8)
+    images_aug = [image] * n_augments
+    images_aug = augment_seq.augment_images(images_aug)
+    return images_aug
+
 
 def resize(input):
     pil_img = Image.fromarray(input)
     resized_phil_img = pil_img.resize((width, height), Image.ANTIALIAS)
     return np.array(resized_phil_img)
+
+
 
 # original label
 '''
@@ -51,6 +73,9 @@ def main():
 
     new_ds = []
 
+    covid_cnt = 0
+
+    # Labelling
     for i in range(len(dataset)):
         resized = resize(img)
         ds = dataset[i]
@@ -59,11 +84,45 @@ def main():
             "img": resized,
             "lab": ds["lab"][3]
         })
-        # plt.imshow(resized, cmap="gray")
-        # plt.show()
 
-    print(new_ds[0])
+        if ds["lab"][3] == 1:
+            covid_cnt += 1
 
+    non_covid_cnt = len(dataset) - covid_cnt
+    dataset = None   # to gc
+
+    # Data augmentation
+    augment_ds = []
+    offset = covid_cnt - non_covid_cnt
+    ratio = -(covid_cnt // -non_covid_cnt)
+    print(f"ratio is {ratio}")
+
+    covid_cnt = non_covid_cnt = 0       # for recalculation
+    for i in range(len(new_ds)):
+        augmented = []
+        if new_ds[i]["lab"] == 1:
+            augmented = augment_image(new_ds[i]["img"])
+            covid_cnt += len(augmented)
+
+            # test augmented images
+            if i == 1:
+                cv.imshow("original", new_ds[i]["img"])
+                cv.waitKey(0)
+                cv.destroyAllWindows()
+                for i in range(len(augmented)):
+                    cv.imshow(f"augmented-{i}", augmented[i])
+                    cv.waitKey(0)
+                    cv.destroyAllWindows()
+        else:
+            if offset > 0:
+                augmented = augment_image(new_ds[i]["img"], ratio)
+                offset -= (len(augmented) - 1)
+            else:
+                augmented = augment_image(new_ds[i]["img"])
+            non_covid_cnt += len(augmented)
+        augment_ds.append(augmented)
+
+    print(f"Covid: {covid_cnt}, others: {non_covid_cnt}")
     # print(type(img))
     # plt.show()
     # csv = pd.DataFrame(dataset.csv)
